5b24dfee42eb4d842239425c8dc504e6870d3bd7
enzomedina <enzomedinacoder@gmail.com>
2023-12-04 01:37:05 -0300

    Ultimo commit

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -6,6 +6,10 @@ import { User } from "src/app/dashboard/pages/users/models";
 import { environment } from "src/environments/environment.local";
 import{MockProvider} from "ng-mocks";
 import { Router } from "@angular/router";
+import { provideMockStore } from "@ngrx/store/testing";
+import { State } from "src/app/store/auth/auth.reducer";
+import { selectAuthUser } from "src/app/store/auth/auth.selectors";
+
 
 
 
@@ -20,9 +24,19 @@ describe('AuthService', ()=>{
                 RouterTestingModule
             ],
             providers:[
-                MockProvider(Router)
+                MockProvider(Router),
+                provideMockStore<State>({
+                initialState:{  
+                    authUser:null,
+                },
+                selectors:[
+                    {
+                        selector:selectAuthUser,value:null
+                    }
+                ]
+
+                })
             ]
-        
         });
         authService=TestBed.inject(AuthService)
         httpController=TestBed.inject(HttpTestingController)
